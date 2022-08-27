29ab8b8ad73de08043670de0dd0802563fe8fd89
melos7n <ernest.melo88@gmail.com>
2022-08-27 12:31:20 -0400

    basic structure is readysolar systems are ready to be deployed

--- src/common/Main.java
+++ src/common/Main.java
@@ -9,6 +9,8 @@ public class Main {
         inception.Sun=1;
         inception.Moon=4;
         inception.Planets=2;
-        
+
+        System.out.println("the basic structure is ready");
+
     }
 }
