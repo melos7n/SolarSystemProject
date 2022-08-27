299b4c195610e5f33f256a98513ff9b69cc12c66
melos7n <ernest.melo88@gmail.com>
2022-08-27 12:29:14 -0400

    solar system Basics are ready

---
+++ src/common/Main.java
@@ -0,0 +1,14 @@
+package common;
+
+public class Main {
+
+    public static void main(String[] arg) {
+
+        solarSystem inception= new solarSystem();
+
+        inception.Sun=1;
+        inception.Moon=4;
+        inception.Planets=2;
+        
+    }
+}
---
+++ src/common/solarSystem.java
@@ -0,0 +1,14 @@
+package common;
+
+public class solarSystem {
+
+
+        int Sun;
+        int Moon;
+        int Planets;
+
+    }
+
+
+
+
